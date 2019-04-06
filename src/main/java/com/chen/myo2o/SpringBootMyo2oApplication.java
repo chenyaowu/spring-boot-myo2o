38ed5610fb1b7ea3fd94268e763bae6de2d7b02e
chenyaowu <chenyaowu15@gmail.com>
2019-04-06 15:02:06 +0800

    Initial commit

---
+++ src/main/java/com/chen/myo2o/SpringBootMyo2oApplication.java
@@ -0,0 +1,13 @@
+package com.chen.myo2o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyo2oApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyo2oApplication.class, args);
+	}
+
+}
